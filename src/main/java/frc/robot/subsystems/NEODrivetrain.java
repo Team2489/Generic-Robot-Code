ceae119a772585d5f6c7ab1f0746b61e2f22806a
aditya917 <52827225+aditya917@users.noreply.github.com>
2020-01-05 22:57:58 -0800

    Added Encoder Velocity

--- src/main/java/frc/robot/subsystems/NEODrivetrain.java
+++ src/main/java/frc/robot/subsystems/NEODrivetrain.java
@@ -8,9 +8,11 @@
 package frc.robot.subsystems;
 
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
@@ -25,15 +27,24 @@ public class NEODrivetrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   public CANSparkMax rightFrontSpark = new CANSparkMax(RobotMap.RIGHT_FRONT_SPARK, MotorType.kBrushless);
-   CANSparkMax rightBackSpark = new CANSparkMax(RobotMap.RIGHT_BACK_SPARK, MotorType.kBrushless);
+         CANSparkMax rightBackSpark = new CANSparkMax(RobotMap.RIGHT_BACK_SPARK, MotorType.kBrushless);
   public CANSparkMax leftFrontSpark = new CANSparkMax(RobotMap.LEFT_FRONT_SPARK,MotorType.kBrushless);
-  CANSparkMax leftBackSpark = new CANSparkMax(RobotMap.LEFT_BACK_SPARK, MotorType.kBrushless);
+         CANSparkMax leftBackSpark = new CANSparkMax(RobotMap.LEFT_BACK_SPARK, MotorType.kBrushless);
+  CANEncoder rightFrontCanEncoder = new CANEncoder(rightFrontSpark);
+  CANEncoder rightBackCanEncoder = new CANEncoder(rightBackSpark);
+  CANEncoder leftFrontCanEncoder = new CANEncoder(leftFrontSpark);
+  CANEncoder leftBackCanEncoder = new CANEncoder(leftBackSpark);
   public NEODrivetrain(){
     rightBackSpark.follow(rightFrontSpark, true);
     leftBackSpark.follow(leftFrontSpark, true);
 
     rightFrontSpark.enableVoltageCompensation(12);
     leftFrontSpark.enableVoltageCompensation(12);
+    rightFrontCanEncoder.getVelocity();
+    leftFrontCanEncoder.getVelocity();
+    rightBackCanEncoder.getVelocity();
+    leftBackCanEncoder.getVelocity();
+    
   }
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
